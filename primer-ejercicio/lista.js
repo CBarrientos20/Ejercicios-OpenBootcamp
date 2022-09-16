76dbda9010ee13b7fe9696ef20720a59f7304f9d
Carla <barrientoscarla190@gmail.com>
2022-09-16 17:48:52 -0300

    lista de datos para ejercicio de  lista y objetos

---
+++ primer-ejercicio/lista.js
@@ -0,0 +1,13 @@
+const datos={
+    nombre: "Carla",
+    edad: 31,
+    eresDesarollador: false,
+    fechaDeNacimiento: new Date("october 20 1990"),
+    libroFavorito:{
+     titulo: "Head First JavaScript Programming",
+     autor: "Eric Freeman & Elizabeth Robson",
+     fechaPublicacion: new Date("March 2014"),
+     url:"http://my.safaribooksonline.com"
+    }
+
+}

2aedf38d0ef80905f7c00a2f0422d4403a02e3a4
Carla <barrientoscarla190@gmail.com>
2022-10-03 15:19:12 -0300

    resolviendo los ejercicios de práctica de JavaScript de Open Bootcamp

---
+++ numbers/numbers.js
@@ -0,0 +1,13 @@
+let miAlturaEnCm = 155;
+let miAlturaEnMts = 1.55;
+
+let miPesoEnKg = 53.3;
+
+let miALturaRedondeada = miAlturaEnMts.toFixed();
+console.log(miALturaRedondeada);
+
+let miPesoRedondeado = Math.round(miPesoEnKg);
+console.log(miPesoRedondeado);
+
+let max_valor_JS = Number.MAX_VALUE;
+let max_valor_mas_uno = max_valor_JS + 1 === Number.MAX_VALUE;

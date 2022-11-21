d5f5c36be68552a75930196d62fed0146b69f8fd
Carla <barrientoscarla190@gmail.com>
2022-11-21 14:20:41 -0300

    entregando ejercicios de sets en el módulo 7 ´trabajando con Sets y Objetos´

---
+++ trabajando-con-sets-y-objetos/conjuntos.js
@@ -0,0 +1,8 @@
+const miFamilia= ["ariel", "luna", "andromeda", "nina", "gregory","terry"];
+
+const miFamiliaSet = new Set(miFamilia)
+console.log(miFamiliaSet)
+miFamiliaSet.add("carla");
+console.log(miFamiliaSet);
+miFamiliaSet.add("javascript")
+console.log(miFamiliaSet)

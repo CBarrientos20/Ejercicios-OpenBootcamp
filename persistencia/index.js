8dd6042df81b3afda2f916974ea6ee5bdeb5339b
Carla <barrientoscarla190@gmail.com>
2023-01-02 12:49:42 -0300

    ejercicio de almacenamiento en navegador resuelto

---
+++ persistencia/index.js
@@ -0,0 +1,17 @@
+let nombre = "carla";
+let apellido = "barrientos";
+
+const datos = {
+    nombre,
+    apellido
+}
+
+// localStorage.setItem("mis datos", JSON.stringify(datos))
+
+// sessionStorage.setItem("datos-sesion", JSON.stringify(datos) )
+
+// console.log(JSON.parse(sessionStorage.getItem("datos-sesion")))
+
+// const now = new Date()
+// document.cookie= `datos=${JSON.stringify(datos)}; expires=${new Date (now.getTime() + 2 * 60000)}`
+// console.log(document.cookie)
